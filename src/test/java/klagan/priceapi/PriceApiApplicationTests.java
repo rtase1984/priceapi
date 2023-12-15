0eedb56a961a0f3d6d40d48380170bb2856eab49
Ramon O Ramirez Tase <ramon.tase@iamzew.org>
2023-12-15 12:26:21 -0300

    Initial commit

---
+++ src/test/java/klagan/priceapi/PriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package klagan.priceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
